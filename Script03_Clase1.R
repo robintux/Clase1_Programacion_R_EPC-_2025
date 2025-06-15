9326e99ecde32ff87f7d872832034a72c1d51666
Abraham Zamudio Chauca <abraham.zamudio@gmail.com>
2025-06-15 22:11:16 +0000

    Algunos paquetes base y populares a tener en cuenta

---
+++ Script03_Clase1.R
@@ -0,0 +1,11 @@
+# paquetes base:
+  # stats : Funciones estadisticas basicas
+  # graphics  : Herramientas para graficos base (estandar)
+  # utils : Utilidades para manipulacion de datos e instalacion de paquetes 
+
+# Paquetes populares
+  # ggplot2
+  # dplyr
+  # caret
+  # shiny
+  # forecast
