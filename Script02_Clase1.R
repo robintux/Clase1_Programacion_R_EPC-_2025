4b4e95b816513ffd791e23f69d3ead8de5e43e82
Abraham Zamudio Chauca <abraham.zamudio@gmail.com>
2025-06-15 22:04:05 +0000

    Agregamos ciertos servicios en linea que ofrecen la capacidad de interpretar codigo del lenguaje de programacion R

--- Script02_Clase1.R
+++ Script02_Clase1.R
@@ -2,3 +2,13 @@
 #### Herramientas en linea para programar con R ####
 # Posit Cloud
 # Notebook de Google Colab
+# Notebook de Kaggle 
+# MyBinder
+# deepnote
+# paperspace
+# SageMaker (AWS)
+# Watson Studio (IBM)
+# CoCalc
+# SageMath
+# mljar
+# labex
