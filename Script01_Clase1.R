32fdbb529128a8a0bb841a97a3e88d52b66d2fac
Abraham Zamudio Chauca <robintux@users.noreply.github.com>
2025-06-15 16:56:59 -0500

    Archivos creados en la infraestructura de posit

---
+++ Script01_Clase1.R
@@ -0,0 +1,27 @@
+# Primer comentario 
+# En esta sesion mostraremos algunas de las capacidades del entorno 
+# de desarrollo integrado (IDE) posit (Ex-Rstudio)
+
+#### Introduccion al Lenguaje de Programacion R  ####
+# R es un lenguaje de programacion y entornos de software libre diseñado especificamente
+# para analisis estadistico, visualizacion de datos y modelado computacional.
+
+
+#### Origen ####
+# Origen es de los 90's 
+# Objetivo inicial : Proporcionar una alternativa flexible y accesible al entorno S
+
+
+#### Reconocimiento actual 
+# Es uno de los lenguajes mas utulizados en el analisis de datos junto con Python
+# Tiene miles de paquetes disponibles a traves de CRAN
+
+
+
+
+
+
+
+
+
+
---
+++ Script02_Clase1.R
@@ -0,0 +1,4 @@
+
+#### Herramientas en linea para programar con R ####
+# Posit Cloud
+# Notebook de Google Colab
